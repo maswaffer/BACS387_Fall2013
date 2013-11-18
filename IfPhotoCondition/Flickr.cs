668fa9aa38bc69286503f65bde9363214cdac62f
Tin Duong <duon9096@bears.unco.edu>
2013-11-18 13:56:08 -0700

    adding flickr service

--- IfPhotoCondition/Flickr.cs
+++ IfPhotoCondition/Flickr.cs
@@ -3,17 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlickrNet;
 // using Flickr .NET HERE...
 namespace IfPhotoCondition
 {
     public class Flickr
     {
 
-        // do something in here.....
+        ///<summary>
+        /// Adding some Code using Flickr API
+        /// here
+        /// </summary>
+        #region
+        // do something in here
+
+
+        #endregion
+
+        // properties here
+        public FlickrNet.Flickr Service {get;set;}
+
+
+         
         // there will be a constructor for flickr here...
         public Flickr()
         {
-
+            Service = new FlickrNet.Flickr();
         }
 
         // go to flickr API get the photo value
