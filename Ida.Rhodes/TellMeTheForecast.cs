5975def7c6f5007f37a35bb44f43a61fda88e598
Jake Neel <neel1785@bears.unco.edu>
2013-12-01 23:04:34 -0700

    Jake N. - Added Sample XML file, Got API calls mostly finished, but Intellisense broke and I commented out changes to preserve work.

--- Ida.Rhodes/TellMeTheForecast.cs
+++ Ida.Rhodes/TellMeTheForecast.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Ida.Rhodes
 {
@@ -10,21 +13,63 @@ namespace Ida.Rhodes
     {
         public Forecast GetForecast(string zipcode)
         {
-            /*
-             * Go to WU API
-             * check condition for zipcode
-             * build Stock object
-             * return
-             */
+            //public List<Forecast> GetForecast()
+            //{
+            //    //These pull the City and state using the ZipCode
+            //    var ZipCode = zipcode;
+
+            //    //This bulds the API call using the apiKey, City, State, etc.
+            //    var apiKey = "9a709d3a540125fd";
+            //    var url = @"http://api.wunderground.com/api/{0}/hourly/q/{1}.xml";
+            //    var urlFixed = string.Format(url, apiKey, ZipCode);
+
+            //    //Calls the API and retrieves the XML file that is parsed.
+            //    var client = new WebClient();
+            //    var response = client.DownloadString(urlFixed);
+            //    var xml = XElement.Parse(response);
+
+            //    //builds forecasts using the parsed XML information
+            //    var forecasts = (from el in xml.Descendants("forecast")
+            //                   select new Forecast
+            //                   {
+            //                       Timestamp = new DateTime(
+            //                                                Convert.ToInt32(el.Descendants("year").First().Value),
+            //                                                Convert.ToInt32(el.Descendants("mon").First().Value),
+            //                                                Convert.ToInt32(el.Descendants("mday").First().Value),
+            //                                                Convert.ToInt32(el.Descendants("hour").First().Value),
+            //                                                Convert.ToInt32(el.Descendants("min").First().Value),
+            //                                                Convert.ToInt32(el.Descendants("sec").First().Value)
+            //                                                ),
+            //                       ChanceOfPrecip = Convert.ToInt32(el.Descendants("pop").FirstOrDefault().Value),
+            //                       HighTemp = Convert.ToInt32(el.Descendants("english").FirstOrDefault().Value)
+            //                   }).ToList();
+            //}
 
             return new Forecast
             {
-                ChanceOfPrecip = 50,
+                ChanceOfPrecip = 0,
                 HighTemp = 15,
                 LowTemp = 40,
                 Timestamp = DateTime.Now,
                 ZipCode = zipcode
             };
+
+            //public int Precipitation()
+            //{
+            //    int rainChance = 
+            //}
+
+            //public string[] GetLocation(string zipcode)
+            //{
+            //    var zipURL = @"http://ziptasticapi.com/{0}";
+            //    var zipURLFixed = string.Format(zipURL,zipcode);
+            //    var zipClient = new WebClient();
+            //    var zipResponse = zipClient.DownloadString(zipURLFixed);
+
+            //    string[] CityState = {"Greeley", "CO"};
+
+            //    return CityState;
+            //}
         }
     }
 }
