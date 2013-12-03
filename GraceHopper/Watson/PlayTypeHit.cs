4cf025036c3285fe6c5538d43e8ca50e239f44fc
Matthew Swaffer <maswaffer@gmail.com>
2013-12-02 19:13:01 -0700

    Commented out most of Watson project so the other teams can work.

--- GraceHopper/Watson/PlayTypeHit.cs
+++ GraceHopper/Watson/PlayTypeHit.cs
@@ -6,29 +6,29 @@ using System.Threading.Tasks;
 
 namespace Watson
 {
-    public class PlayTypeHit : ICriteria<Play>
-    {
-        public string Offense { 
-              get { return "Was a Offensive Play made?"; }
-            set {   }
-        }
+    //public class PlayTypeHit : ICriteria<Play>
+    //{
+    //    public string Offense { 
+    //          get { return "Was a Offensive Play made?"; }
+    //        set {   }
+    //    }
          
         
-        public bool Check(Play Offense)
-        { 
-            if (value.Play.Equals(PlayCode))
-                 {
-                    match = true;
-                }
-             else
-            {
-                match = false; 
-            }
-            return match;
-        }
+    //    public bool Check(Play Offense)
+    //    { 
+    //        if (value.Play.Equals(PlayCode))
+    //             {
+    //                match = true;
+    //            }
+    //         else
+    //        {
+    //            match = false; 
+    //        }
+    //        return match;
+    //    }
 
-        public string playcode { get; set; }
+    //    public string playcode { get; set; }
 
-    }
+    //}
     
 }
