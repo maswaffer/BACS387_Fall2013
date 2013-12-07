59c13e83b4adde8bfc3781895ccc5e2f24cea54b
Tin Duong <duon9096@bears.unco.edu>
2013-12-07 00:12:48 -0700

    Then Action

---
+++ ThenShowPhotos/ShowPhotosAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+namespace ThenShowPhotos
+{
+    public class ShowPhotosAction:IAction
+    {
+        public void SendNotification(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
